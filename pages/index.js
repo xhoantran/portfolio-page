da631abb7877ff17cfe595bf76ad8c3981b1b488
xhoantran <tranxuanhoan2833@gmail.com>
2021-10-18 11:54:58 -0400

    Commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <div>Hoan Yêu Thy</div>;
+export default function LandingPage() {
+  return (
+    <div>
+      <div>
+        <Head>
+          <title>My portfolio</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+
+        <p>Hello world!</p>
+      </div>
+    </div>
+  );
 }
