48d9abd2e4294d1f09b4961c5d1651f2f1977699
xhoantran <tranxuanhoan2833@gmail.com>
2021-10-19 13:13:25 -0400

    - Add SVG Home 1 component - Add Layout to the homepage

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,37 @@ import Head from "next/head";
 import styled from "@emotion/styled";
 
 import Header from "../components/Headers";
+import SVGHome1 from "../components/SVG/SVGHome1";
+
+const ContentWrapper = styled.div`
+  margin: 180px 125px 0 125px;
+`;
 
 const ContentContainer = styled.div`
-  margin: 28px 125px 0 125px;
+  display: flex;
 `;
+
+const TextContainer = styled.div`
+  margin-left: 4rem;
+  max-width: 480px;
+`;
+
+const AttractText = styled.div`
+  font-weigth: 500;
+  font-size: 36px;
+`;
+
+const SmallAttractText = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+`;
+
+const StrongText = styled.div`
+  display: inline-block;
+  font-weight: bold;
+`;
+
+const GraphContainer = styled.div``;
 
 export default function LandingPage() {
   return (
@@ -16,8 +43,22 @@ export default function LandingPage() {
       </Head>
 
       <Header />
-
-      <ContentContainer></ContentContainer>
+      <ContentWrapper>
+        <ContentContainer>
+          <TextContainer>
+            <AttractText>Hoan Tran</AttractText>
+            <AttractText>Software Engineer</AttractText>
+            <AttractText>AI Engineer</AttractText>
+            <SmallAttractText>
+              Welcome to my portfolio! I am an enginner that use my skills to
+              solve problem <StrongText>efficiency</StrongText>
+            </SmallAttractText>
+          </TextContainer>
+          <GraphContainer>
+            <SVGHome1 />
+          </GraphContainer>
+        </ContentContainer>
+      </ContentWrapper>
     </div>
   );
 }
